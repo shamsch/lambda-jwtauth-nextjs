6e3d7ebd350e6e390bc8ff5b10edca58ba81c952
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-27 10:20:07 +0300

    comment

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,22 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 export default function handler(req, res) {
-    // verify jwt token from request
-    const token = req.headers.authorization;
+	// verify jwt token from request
+	const token = req.headers.authorization;
 
-    if (!token) {
-        res.status(401).json({ error: "Unauthorized" });
-        return;
-    }
+	if (!token) {
+		res.status(401).json({ error: "Unauthorized" });
+		return;
+	}
 
-    // //get token from bearer token
-    const bearerToken = token.split(" ")[1];
-    //verify token
-    const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
-    if (!decoded) {
-        res.status(401).json({ error: "Unauthorized" });
-        return;
-    }
-    const { email, id } = decoded;
-    res.status(200).json({ email, id });
-  }
+	//get token from bearer token
+	const bearerToken = token.split(" ")[1];
+	//verify token
+	const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET);
+	if (!decoded) {
+		res.status(401).json({ error: "Unauthorized" });
+		return;
+	}
+	const { email, id } = decoded;
+	res.status(200).json({ email, id });
+}
