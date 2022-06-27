e67fbf2b9e47d189e69b275a764d6831ff10b584
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-27 11:54:46 +0300

    route guarding

--- pages/logout.js
+++ pages/logout.js
@@ -1,14 +1,31 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Logout = () => {
-    const router = useRouter();
+	const router = useRouter();
 
-    const handleClick = () => {
-        router.push("/");
-        sessionStorage.clear();
-    };
-    
-    return <button className="btn" onClick={handleClick}>Click to log out</button>;
+	let token = null;
+
+	if (typeof window !== "undefined") {
+		token = sessionStorage.getItem("token");
+	}
+
+	useEffect(() => {
+		if (!token) {
+			router.push("/login");
+		}
+	}, [token]);
+
+	const handleClick = () => {
+		router.push("/");
+		sessionStorage.clear();
+	};
+
+	return (
+		<button className="btn" onClick={handleClick}>
+			Click to log out
+		</button>
+	);
 };
 
 export default Logout;
--- pages/secret.js
+++ pages/secret.js
@@ -1,6 +1,22 @@
 import useSWR from "swr";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Secret = () => {
+	const router = useRouter();
+
+	let token = null;
+
+	if (typeof window !== "undefined") {
+		token = sessionStorage.getItem("token");
+	}
+
+	useEffect(() => {
+		if (!token) {
+			router.push("/login");
+		}
+	}, [token]);
+
 	const { data, error } = useSWR("/api/", (url) =>
 		fetch(url, {
 			headers: {
