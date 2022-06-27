335f74aef266b642f26d2ad3526a3b067b85fb6e
Shamsur Chowdhury <shamsur.chowdhury@crowdsorsa.com>
2022-06-27 11:39:55 +0300

    password strength check implemented

--- pages/signup.js
+++ pages/signup.js
@@ -1,90 +1,105 @@
 import { useState } from "react";
 import { Notification } from "../components/Notification";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
+import PasswordStrengthBar from "react-password-strength-bar";
 
 const Signup = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState(null);
-    const [success, setSuccess] = useState(false);
-    
-    const router = useRouter();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(null);
+	const [success, setSuccess] = useState(false);
+	const [passwordStrength, setPasswordStrength] = useState(0);
 
+	const router = useRouter();
 
-    const API = process.env.API;
+	const API = process.env.API;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        handleSignUp(email, password);
-    };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (passwordStrength >= 3) {
+			handleSignUp(email, password);
+		} else {
+			setError("Password is not strong enough");
+		}
+	};
 
-    const handleSignUp = (email, password) => {
-        fetch(`${API}/signup`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email,
-                password,
-            }),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.error) {
-                    setError(data.error);
-                } 
-                if(data.id){
-                    setSuccess(true); 
-                    setTimeout(() => {
-                        setSuccess(false)
-                        router.push("/login");
-                    }, 3000);
-                }
-            })
-            .catch((err) => {
-                setError(err);
-            });
-    };
+	const handleSignUp = (email, password) => {
+		fetch(`${API}/signup`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				email,
+				password,
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.error) {
+					setError(data.error);
+				}
+				if (data.id) {
+					setSuccess(true);
+					setTimeout(() => {
+						setSuccess(false);
+						router.push("/login");
+					}, 3000);
+				}
+			})
+			.catch((err) => {
+				setError(err);
+			});
+	};
 
-    const handleError = (timeout) => {
-        setTimeout(() => {
-            setError(null);
-        }, timeout);
-        return <Notification message={error} />;
-    };
+	const handleError = (timeout) => {
+		setTimeout(() => {
+			setError(null);
+		}, timeout);
+		return <Notification message={error} />;
+	};
 
-    return (
-        <>
-            <h1>Signup</h1>
-            {error ? handleError(3000) : null}
-            {success ? <Notification message={"Successful sign up. You will be redirected to log in shortly."} /> : null}
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <label>
-                    Your Email:
-                    <br />
-                    <input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        type="text"
-                        name="email"
-                    />
-                </label>
-                <label>
-                    Choose Password:
-                    <br />
-                    <input
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                        name="password"
-                    />
-                </label>
+	return (
+		<>
+			<h1>Signup</h1>
+			{error ? handleError(3000) : null}
+			{success ? (
+				<Notification
+					message={
+						"Successful sign up. You will be redirected to log in shortly."
+					}
+				/>
+			) : null}
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<label>
+					Your Email:
+					<br />
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						type="text"
+						name="email"
+					/>
+				</label>
+				<label>
+					Choose Password:
+					<br />
+					<input
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						type="password"
+						name="password"
+					/>
+					<PasswordStrengthBar
+						password={password}
+						onChangeScore={(score) => setPasswordStrength(score)}
+					/>
+				</label>
 
-                <button className="btn">Signup</button>
-            </form>
-        </>
-    );
+				<button className="btn">Signup</button>
+			</form>
+		</>
+	);
 };
 
 export default Signup;
